dc326f3e314f6539ec3945a1a8830d2e1e842a6a
Aildson Ferreira <aildson.ferreira@icloud.com>
2025-12-04 20:16:35 -0300

    build: setup next configs for remapping app routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => [
+    {
+      source: "/",
+      destination: "/dashboard",
+    },
+    {
+      source: "/dashboard",
+      destination: "/",
+    },
+  ],
 };
 
 export default nextConfig;
