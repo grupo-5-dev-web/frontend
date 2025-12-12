58c06224ecf68a759e1e0619eb8ae753ec2ac680
Aildson Ferreira <aildson.ferreira@icloud.com>
2025-12-12 15:07:07 -0300

    feat: integrate /register page with api

---
+++ api/user/create.ts
@@ -0,0 +1,68 @@
+import axios from "axios";
+
+export type User = {
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+  user_type?: "admin" | "user";
+  department?: string;
+};
+
+export const create = ({
+  name,
+  phone = "",
+  email,
+  password,
+  user_type = "user",
+  department = "",
+}: User) => {
+  const getPermissions = () => {
+    switch (user_type) {
+      case "admin":
+        return {
+          can_book: true,
+          can_manage_resources: true,
+          can_manage_users: true,
+          can_view_all_bookings: true,
+        };
+      case "user":
+        return {
+          can_book: true,
+          can_manage_resources: false,
+          can_manage_users: false,
+          can_view_all_bookings: false,
+        };
+      default:
+        return {
+          can_book: false,
+          can_manage_resources: false,
+          can_manage_users: false,
+          can_view_all_bookings: false,
+        };
+    }
+  };
+
+  return axios
+    .post(
+      "https://user-service-production-6b5a.up.railway.app/users/",
+      {
+        name,
+        phone,
+        email,
+        password,
+        user_type,
+        department,
+        is_active: true,
+        permissions: getPermissions(),
+        tenant_id: "a838726b-699f-45b5-9a07-5ee092ae84f2", // TODO: Test tenant_id, should be handled properly
+      },
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+    .then((response) => response.data)
+    .catch((error) => {
+      throw new Error(error.response?.data?.message || "User creation failed");
+    });
+};
