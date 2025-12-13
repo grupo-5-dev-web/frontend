f4b3f25d30280bad3950787146ef7b1b23cd1234
Aildson Ferreira <aildson.ferreira@icloud.com>
2025-12-13 13:02:16 -0300

    refactor: adjust imports spacing

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Layout } from "@/components/ui/layout";
+import { Layout } from "@/components/layout";
 
 import "../../styles/globals.css";
+
 import { UserProvider } from "@/contexts/UserContext";
 
 const geistSans = Geist({
--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "../../styles/globals.css";
+
 import { UserProvider } from "@/contexts/UserContext";
 
 const geistSans = Geist({
